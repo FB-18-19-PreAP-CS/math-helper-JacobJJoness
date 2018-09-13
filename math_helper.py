c72760a0ffb72ac9ca6cc0f248fb410e803341ec
JONES.JACOB <2200361@fbisd.flourbluffschools.net>
2018-09-13 13:37:25 -0500

    my teacher made me do his

--- math_helper.py
+++ math_helper.py
@@ -1,14 +1,16 @@
-from math import *
+import math
 
-
+''' Welcome to the math helper.
+'''
 def quadratic_formula(a,b,c):
-    x= (b**2)-(4*a*c)
-    math.sqrt(x)
+    cookies = (b**2)-(4*a*c)
+    math.sqrt(cookies)
+    print(cookies)
     
     
     
     
     
     
-quadratic_formula(1,2,3)
+quadratic_formula(3,4,5)
     
