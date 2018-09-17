03da88d1306cd9579732feb993b5c8da7bcd0894
JONES.JACOB <2200361@fbisd.flourbluffschools.net>
2018-09-17 13:48:54 -0500

    Pythagorean theoreum is working correctly

--- math_helper.py
+++ math_helper.py
@@ -1,25 +1,45 @@
-import math
+import math 
 
 ''' Welcome to the math helper.
 '''
-def quadratic_formula(a,b,c):
-    x = (b**2)-(4*a*c)
-    math.sqrt(x)
-    print(x)
-    
-    
-def pythag_theo(a,b):
-    c = ((a**2)+(b**2))
-    d = math.sqrt(c)
-    print(d)
-    
-pythag_theo(4,4)
-    
-    
-    
 
     
     
+def pythag_theo(a,b):
+    ''' This function calculates the pythagorean theorem for the user
+        while also checking for impossible inputs
+        
+        >>> pythag_theo(3,4)
+        5.0
+    
+        >>> pythag_theo(0,5)
+        Traceback (most recent call last):
+        ...
+        ValueError: a or b must be positive
+         
+        >>> pythag_theo(-5,5)
+        Traceback (most recent call last):
+        ...
+        ValueError: a or b must be positive
+    
+    '''
+    if a<= 0 or b <= 0:
+        raise ValueError("a or b must be positive")
+    c = ((a**2)+(b**2))
+    return math.sqrt(c)
+
+
+    
+
+def main():
+    #pythag_theo
+    pass
+
+    
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    #main()
     
     
     
